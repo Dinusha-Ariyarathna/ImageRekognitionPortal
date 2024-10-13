e92cb639133525038374e233ece4a0ec95f85505
dinusha_d <dinushaariyarathna2@gmail.com>
2024-10-13 08:40:01 +0530

    Created a controller to check status.

--- src/main/java/com/example/portal/service/controller/ImageAnalysisController.java
+++ src/main/java/com/example/portal/service/controller/ImageAnalysisController.java
@@ -7,6 +7,7 @@ import com.example.portal.service.service.impl.RecognitionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +39,10 @@ public class ImageAnalysisController {
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hello World";
     }
 }
--- src/main/java/com/example/portal/service/service/impl/RecognitionServiceImpl.java
+++ src/main/java/com/example/portal/service/service/impl/RecognitionServiceImpl.java
@@ -66,4 +66,6 @@ public class RecognitionServiceImpl implements RecognitionService {
         }
         return false;
     }
+
+
 }
